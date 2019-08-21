c47e19170bd86e6ce3f463bc278df84345af240b
np01389 <np01389@CEIT2553214X027.ad.georgiasouthern.edu>
2019-08-21 16:35:00 -0400

    First commit with my name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -3,7 +3,7 @@ package assignment;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello, my name is D'angelo Plummer");
 		
 		//add a statement to display your name in the console
 		
